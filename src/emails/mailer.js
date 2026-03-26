61822be4a4203751a85ceac3b29ed9700679f5d6
Alberto Alas <oa3973392@gmail.com>
2026-03-25 23:31:16 -0600

    ultimos cambios

--- src/emails/mailer.js
+++ src/emails/mailer.js
@@ -3,12 +3,15 @@ import dotnev from 'dotenv'
 dotnev.config()
 
 const transporter = nodemailer.createTransport({
-    service: 'smtp.gmail.com',
+    host: "smtp.gmail.com", 
     port: 587,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     },
     family: 4
 });
