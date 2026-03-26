826e7e4e80181bd82547cae0c9c482226798a1bb
Alberto Alas <oa3973392@gmail.com>
2026-03-25 23:19:32 -0600

    Bugs arreglados

--- src/emails/mailer.js
+++ src/emails/mailer.js
@@ -4,10 +4,13 @@ dotnev.config()
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    family: 4
 });
 
 export default transporter
